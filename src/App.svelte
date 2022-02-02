ed9986a361b7dfb708f259e626e1e25acf506537
Aleksandrs Domkovs <14880089+berlyozzy@users.noreply.github.com>
2022-02-02 22:18:49 +0000

    fixing relative file links in index.html part 3: Le Final (forgot to fix the url for images)

--- src/App.svelte
+++ src/App.svelte
@@ -14,7 +14,7 @@
 <div class="background-image is-unselectable" style="left: 2vw; bottom: 1vh; width: 35%;">
 	<figure class="image">
 		<!-- svelte-ignore a11y-missing-attribute -->
-		<img src="/images/undraw_handcrafts_tree.svg">
+		<img src="/omni-jira/images/undraw_handcrafts_tree.svg">
 	</figure>
 </div>
 {/if}
@@ -22,7 +22,7 @@
 <div class="background-image is-unselectable" style="top: 1vh; right: 1vw; width: 20%;">
 	<figure class="image">
 		<!-- svelte-ignore a11y-missing-attribute -->
-		<img src="/images/undraw_handcrafts_planet.svg">
+		<img src="/omni-jira/images/undraw_handcrafts_planet.svg">
 	</figure>
 </div>
 
